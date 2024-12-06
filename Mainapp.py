d92800d442c6f8730147112ded14309329381124
NeilConZ <140559676+NeilConZ@users.noreply.github.com>
2024-12-06 18:27:00 +0000

    got the buttons working

--- Mainapp.py
+++ Mainapp.py
@@ -1,83 +1,128 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLabel, QLineEdit, QTableWidget, QTableWidgetItem, QTabWidget, QDialog, QFormLayout, QMessageBox, QStackedWidget, QSizePolicy
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLabel, QLineEdit, QTableWidget, QTableWidgetItem, QTabWidget, QDialog, QFormLayout, QMessageBox, QStackedWidget, QSizePolicy, QHBoxLayout
 from PySide6.QtCore import Qt
 
 class MainScreen(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        #Layout and size
         self.setWindowTitle("Greener-Bikes")
-        self.setGeometry(100,100,800,600)
-        self.label = QLabel("Welcome To The Greener-Bikes App!")
-        self.label.setStyleSheet("font-size:40px;")
-
-        main_widget = QWidget()
-        self.setCentralWidget(main_widget)
-        main_layout = QVBoxLayout(main_widget)
-
-        main_layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
-        main_layout.addStretch()
-
-        #setting up buttons
-        self.inventory_management_button = QPushButton("Inventory Management")
-        self.inventory_management_button.setFixedSize(200,100)
-
-
-        self.production_workflow_button = QPushButton("Production Workflow")
-        self.production_workflow_button.setFixedSize(200,100)
-
-
-        self.order_management_button = QPushButton("Order Management")
-        self.order_management_button.setFixedSize(200,100)
-
-        #connecting buttons to pages
-        self.inventory_management_button.clicked.connect(self.inventory_management_page)
-        self.production_workflow_button.clicked.connect(self.production_workflow_page)
-        self.order_management_button.clicked.connect(self.order_management_page)
-
-        #addng buttons to page
-        main_layout.addWidget(self.inventory_management_button, alignment=Qt.AlignmentFlag.AlignCenter)
-        main_layout.addWidget(self.production_workflow_button, alignment=Qt.AlignmentFlag.AlignCenter)
-        main_layout.addWidget(self.order_management_button, alignment=Qt.AlignmentFlag.AlignCenter)
-
-        main_layout.addStretch()
+        self.setGeometry(100, 100, 800, 600)
 
         # setting up the pages
         self.pages = QStackedWidget()
-        main_layout.addWidget(self.pages)
 
+        #creating the pages
+        self.main_page = self.main_menu_page()
         self.inventory_page = self.inventory_management_page()
         self.order_page = self.order_management_page()
         self.production_page = self.production_workflow_page()
 
+        #add pages to the stacked widget
+        self.pages.addWidget(self.main_page)
         self.pages.addWidget(self.inventory_page)
         self.pages.addWidget(self.production_page)
         self.pages.addWidget(self.order_page)
 
+        self.setCentralWidget(self.pages)
+
+    def main_menu_page(self):
+        page = QWidget()
+        layout = QVBoxLayout(page)
+
+        button_layout = QHBoxLayout(page)
+        button_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        label = QLabel("Welcome To The Greener-Bikes App!")
+        label.setStyleSheet("font-size:40px;")
+        layout.addWidget(label, alignment=Qt.AlignmentFlag.AlignHCenter| Qt.AlignmentFlag.AlignTop)
+
+        #inventory button
+        inventory_button = QPushButton("Inventory Management")
+        inventory_button.setFixedSize(300, 100)
+        inventory_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.inventory_page))
+        button_layout.addWidget(inventory_button, alignment= Qt.AlignmentFlag.AlignCenter)
+
+        production_button = QPushButton("Production Workflow")
+        production_button.setFixedSize(300, 100)
+        production_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.production_page))
+        button_layout.addWidget(production_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        order_button = QPushButton("Order Management")
+        order_button.setFixedSize(300, 100)
+        order_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.order_page))
+        button_layout.addWidget(order_button, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        layout.addLayout(button_layout)
+        return page
 
     def inventory_management_page(self):
 
         page = QWidget()
         layout = QVBoxLayout()
+
+        top_bar_layout = QHBoxLayout()
+        top_bar_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+
         label = QLabel("Inventory Management")
-        layout.addWidget(label)
+        label.setStyleSheet("font-size:40px;")
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        top_bar_layout.addWidget(label)
+
+
+        back_button = QPushButton("Back")
+        back_button.setFixedSize(200,100)
+        back_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.main_page))
+        top_bar_layout.addWidget(back_button)
+
+        layout.addLayout(top_bar_layout)
+        page.setLayout(layout)
         return page
 
     def production_workflow_page(self):
         page = QWidget()
         layout = QVBoxLayout()
-        label = QLabel("Production")
-        layout.addWidget(label)
+
+        top_bar_layout = QHBoxLayout()
+        top_bar_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        label = QLabel("Production Workflow")
+        label.setStyleSheet("font-size:40px;")
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        top_bar_layout.addWidget(label)
+
+        back_button = QPushButton("Back")
+        back_button.setFixedSize(200, 100)
+        back_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.main_page))
+        top_bar_layout.addWidget(back_button)
+
+        layout.addLayout(top_bar_layout)
+        page.setLayout(layout)
         return page
 
     def order_management_page(self):
         page = QWidget()
         layout = QVBoxLayout()
+
+        top_bar_layout = QHBoxLayout()
+        top_bar_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
         label = QLabel("Order Management")
-        layout.addWidget(label)
+        label.setStyleSheet("font-size:40px;")
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        top_bar_layout.addWidget(label)
+
+        back_button = QPushButton("Back")
+        back_button.setFixedSize(200, 100)
+        back_button.clicked.connect(lambda: self.pages.setCurrentWidget(self.main_page))
+        top_bar_layout.addWidget(back_button)
+
+        layout.addLayout(top_bar_layout)
+        page.setLayout(layout)
         return page
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
